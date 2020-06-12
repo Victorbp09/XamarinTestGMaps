53f2739fe07a7d5571aa8ec94a008e8c1ff6e300
Victorbp09 <vbeltran09@hotmail.com>
2020-06-12 12:00:38 +0200

    Add project files.

---
+++ TestGMaps/TestGMaps/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.GoogleMaps;
+
+namespace TestGMaps
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            //var defaultPin = new Pin { Type = PinType.Place, Label = "This is my home", Address = "Here", Position = new Position(-23.68, -46.87) };
+            //map.Pins.Add(defaultPin);
+        }
+    }
+}
